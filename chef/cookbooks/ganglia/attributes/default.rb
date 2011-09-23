0c24d380e5328e41ddf05041387fa39428ba5f31
Greg Althaus <galthaus@austin.rr.com>
2011-09-22 19:45:38 -0500

    FIx module path for ubuntu ganglia install.

--- chef/cookbooks/ganglia/attributes/default.rb
+++ chef/cookbooks/ganglia/attributes/default.rb
@@ -1,5 +1,11 @@
 #
 # Default attributes for system.
 #
+def redhat_platform?
+  ["redhat","centos","fedora"].include?(platform)
+end
+
+default[:ganglia][:module_path] = "/usr/lib/ganglia/"
+default[:ganglia][:module_path] = "" if redhat_platform?
 default[:ganglia][:interface_eval] = '#{"eth0"}'
 
