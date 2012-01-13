d021bbff38159f24df87ac3774d07377104bb5ba
Victor Lowther <victor_lowther@dell.com>
2012-01-13 13:32:41 -0600

    Update Ganglia RPM build process to work with updates in the main Crowbar repository

--- build.sh
+++ build.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-mkdir -p /usr/src/redhat/SOURCES && cd /usr/src/redhat/SOURCES
-rpm2cpio /mnt/current_os/pkgs/ganglia-3.1.7-3.fc15.src.rpm | cpio -id
-rpmbuild -bb ganglia.spec
-cd /usr/src/redhat/RPMS
-find . -type f -name '*.rpm' -exec cp '{}' /mnt/current_os/pkgs ';'
+GANGLIA_RPMS=(ganglia-gmetad-3.1.7-3.x86_64.rpm \
+    ganglia-gmond-3.1.7-3.x86_64.rpm \
+    ganglia-gmond-python-3.1.7-3.x86_64.rpm \
+    ganglia-3.1.7-3.x86_64.rpm \
+    ganglia-web-3.1.7-3.x86_64.rpm)
+
+bc_needs_build() {
+    for pkg in ${GANGLIA_RPMS[@]}; do
+	[[ -f $BC_CACHE/$OS_TOKEN/pkgs/$pkg ]] && continue
+	return 0
+    done
+    return 1
+}
+
+bc_build() {
+    sudo cp "$BC_DIR/build_in_chroot.sh" "$CHROOT/tmp"
+    in_chroot /tmp/build_in_chroot.sh
+}
